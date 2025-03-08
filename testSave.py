7c889364712c30608106808fd0a7d4833837d714
Maksym Shablovskyy <mshablovskyy@gmail.com>
2025-03-08 20:49:58 +0100

    updated file to work with new WebScrape

--- testSave.py
+++ testSave.py
@@ -2,12 +2,22 @@ import WebScrape
 import Data
 
 if __name__ == "__main__":
-    id = "135759416"
+# for id in Data.get_ids():
+    id = "74463010"
     product = WebScrape.Page(id)
-    name = product.get_product_name()
-    if name:
-        comments = product.format_comments()
-    else:
-        comments = None
+    if not product.connect()["code"]:
+        name = product.get_product_name()
+        if not name["code"]:
+            name = name["result"]
+            amount = product.get_comments_amount()
+            if not amount["code"]:
+                amount = amount["result"]
+            else: amount = None
+            comments = product.format_comments()
+            if not comments["code"]:
+                comments = comments["result"]
+            else:
+                comments = None
         
-    Data.save(id, name, comments)
+            Data.save(id, name, amount, comments)
+        else: print("bad code")
